8f89506e12374d20ac512af9a09bc40cde885faa
nain12 <nainysewaney@gmail.com>
2018-09-14 15:54:13 +0530

    Spring First

---
+++ HelloSpring/src/com/practice/Spring/HelloWorld.java
@@ -0,0 +1,15 @@
+package com.practice.Spring;
+
+public class HelloWorld {
+
+	private String message;
+
+	public void getMessage() {
+		System.out.println("Your message: "+ message);
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+}
---
+++ HelloSpring/src/com/practice/Spring/MainApp.java
@@ -0,0 +1,14 @@
+package com.practice.Spring;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		ApplicationContext context = new ClassPathXmlApplicationContext("Bean.xml");
+		HelloWorld obj = (HelloWorld) context.getBean("helloworld");
+		obj.getMessage();
+	}
+
+}
