fd67f3379b0505e487163f9583b045aa1ee3df0c
nain12 <nainysewaney@gmail.com>
2018-09-14 22:07:58 +0530

    SoundSystemXMLConfig

--- SoundSystemJavaConfig/src/soundsystem/CDPlayerConfig.java
+++ SoundSystemJavaConfig/src/soundsystem/CDPlayerConfig.java
@@ -10,4 +10,23 @@ public class CDPlayerConfig {
 	public CompactDisc sgtPeppers(){
 		return new SgtPeppers();
 	}
+	
+	//Method 1
+	@Bean
+	public CDPlayer cdPlayer(){
+		return new CDPlayer(sgtPeppers());
+	}
+	
+	//Method 2  - Constuctor injection
+	public CDPlayer cdPlayer(CompactDisc cd){
+		return new CDPlayer(cd);
+	}
+	
+	//Method 3 - Setter injection
+	public CDPlayer CDPlayer(CompactDisc cd){
+		CDPlayer cdp = new CDPlayer(cd);
+		cdp.setCd(cd);
+		return cdp;
+		
+	}
 }
--- SoundSystemXMLConfig/src/soundsystemtest/CDPlayerTest.java
+++ SoundSystemXMLConfig/src/soundsystemtest/CDPlayerTest.java
@@ -9,13 +9,11 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-
-import soundsystem.CDPlayerConfig;
 import soundsystem.CompactDisc;
 import soundsystem.MediaPlayer;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = CDPlayerConfig.class)
+@ContextConfiguration(locations = "/Bean.xml")
 public class CDPlayerTest {
 
 	@Rule
