5e76badefdcfb9b6d98d3d7e79c7ad2cd429baa0
nain12 <nainysewaney@gmail.com>
2018-09-17 00:06:55 +0530

    XML java config

---
+++ MixSoundSystemXMLJavaConfig/src/soundsystemtest/CDPlayerTest.java
@@ -0,0 +1,43 @@
+package soundsystemtest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.contrib.java.lang.system.SystemOutRule;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import soundsystem.CompactDisc;
+import soundsystem.MediaPlayer;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "/Bean.xml")
+public class CDPlayerTest {
+
+	@Rule
+	public final SystemOutRule systemOutRule = new SystemOutRule().enableLog();
+
+	@Autowired
+	private MediaPlayer player;
+
+	@Autowired
+	private CompactDisc cd;
+
+	@Test
+	public void cdShouldNotBeNull() {
+		assertNotNull(cd);
+	}
+
+	@Test
+	public void play() {
+		player.play();
+		assertEquals("Playing Sgt. Pepper's Lonely Hearts Club Band by The Beatles\r\n"
+				+ "Tracks: Sgt. Pepper's Lonely Hearts Club Band\r\n"
+				+ "Tracks: With a Little Help from My Friends\r\n"
+				+ "Tracks: Lucy in the Sky with Diamonds\r\n"
+				+ "Tracks: Getting Better\r\n"
+				+ "Tracks: Fixing a Hole\r\n", systemOutRule.getLog());
+	}
+}
