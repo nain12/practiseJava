ce812973423a94ff75c03aee7d339fa299d3b0e9
nain12 <nainysewaney@gmail.com>
2018-09-19 18:42:48 +0530

    Blank Disc

--- SoundSystemXMLConfig/src/soundsystem/BlankDisc.java
+++ SoundSystemXMLConfig/src/soundsystem/BlankDisc.java
@@ -4,6 +4,30 @@ import java.util.List;
 
 public class BlankDisc implements CompactDisc {
 
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getArtist() {
+		return artist;
+	}
+
+	public void setArtist(String artist) {
+		this.artist = artist;
+	}
+
+	public List<String> getTracks() {
+		return tracks;
+	}
+
+	public void setTracks(List<String> tracks) {
+		this.tracks = tracks;
+	}
+
 	private String title;
 	private String artist;
 	private List<String> tracks;
